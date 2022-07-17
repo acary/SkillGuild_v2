d8fc9d5bf1154a2e4feacc05a66e9542cd38c544
Andy Cary <info@andycary.com>
2022-07-17 08:16:54 -0400

    update gradle

--- SkillGuild/src/test/java/com/skilldistillery/skillguild/controllers/CategoryControllerIntegrationTest.java
+++ SkillGuild/src/test/java/com/skilldistillery/skillguild/controllers/CategoryControllerIntegrationTest.java
@@ -4,12 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -42,16 +40,18 @@ public class CategoryControllerIntegrationTest {
 	@Test
 	public void testCreateCategory() throws Exception {
 
+		// given
 		Category category = new Category();
+		category.setId(0);
 		category.setName("Software Engineering");
 		category.setDescription("Activities and interests related to the Software Engineering discipline");
 		category.setImgUrl("https://images.unsplash.com/photo-1573495627361-d9b87960b12d");
-
 		when(this.categoryService.create(any(Category.class))).thenReturn(category);
 
+		// when
 		Category newCategory = categoryController.create(req, res, category);
 
-		System.out.println(newCategory);
+		// then
 		assertNotNull(newCategory.getName());
 	}
 
